a59a9ca2dfed01e44406d15669c0a6eda9daff29
pradeep <pradeep@mini.local>
2022-12-12 00:00:07 -0700

    added latest changes to submodule

--- restaurant.ts
+++ restaurant.ts
@@ -1,5 +1,28 @@
-function createOrder(order: string) {
-    return "Order is, " + order;
+import {Order, Currency} from "./gitSubModuleSharedLib/merchant"
+
+function createOrder(order: Order) {
+    return "Order is, " + JSON.stringify(order);
+}
+const newOrder: Order = {
+    id: "123",
+    status: "preparing",
+    payment: {
+        amount: 12,
+        currency: Currency.CAD
+    },
+    items: [
+        {
+            id: "123",
+            name: "noodles",
+            itemCategory: "lunch",
+            price: 123,
+            unit: 2,
+            createdAt: Date.now(),
+            updatedAt: Date.now()
+        }
+    ],
+    createdAt: Date.now(),
+    updatedAt: Date.now()
 }
 
-document.body.textContent = createOrder("test");
+console.log(createOrder(newOrder));
